f7befcabbdb0e84326b00ab2b145cc0d16022563
Grecia Lorena Morales Castillo <cdmx2510@amerike.edu.mx>
2023-10-19 04:54:02 -0600

    informacion terminada

--- FundamentosWeb/js/main.js
+++ FundamentosWeb/js/main.js
@@ -1,3 +1,94 @@
 document.getElementById('toggle-mode').addEventListener('click', function() 
 {
   document.body.classList.toggle('dark-mode');});
+
+  $btnima.addEventListener("click",(e)=>{
+    let $bgimg = document.querySelector("#headerimg");
+  });
+
+  console.log("Hola Mundo");
+
+  let $linkGithub = document.querySelector("#link-github");
+  console.log($linkGithub);
+  
+  $linkGithub.href =
+    "https://github.com/Greyslor/ArticuloInvestigacion";
+  $linkGithub.target = "_blank";
+  
+  let nombre = "Greys";
+  let $nombre = document.querySelector("#nombre");
+  
+  $nombre.innerHTML = nombre;
+  
+  //Repaso de AJAX y JSON
+  async function obtenerDatos() {
+    let respuesta = await fetch("js/data.json");
+    let datos = await respuesta.json();
+  
+    let $info = document.querySelector("#info");
+  
+    $info.innerHTML = `
+      <table border="1">
+        <tr>
+          <td>Nombre:</td>
+          <td>${datos.nombre}</td>
+        </tr>
+        <tr>
+          <td>Edad:</td>
+          <td>${datos.edad}</td>
+        </tr>
+        <tr>
+          <td>Es Estudiante:</td>
+          <td>${datos.estudiante}</td>
+        </tr>
+        <tr>
+          <td>Materias:</td>
+          <td>${datos.materias}</td>
+        </tr>
+        <tr>
+          <td>Correo:</td>
+          <td>${datos.contacto.correo}</td>
+        </tr>
+        <tr>
+          <td>Movil:</td>
+          <td>${datos.contacto.movil}</td>
+        </tr>
+      </table>
+    `;
+  
+    console.log(datos);
+  }
+  
+  obtenerDatos();
+  
+  async function obtenerDatosGitHub() {
+    let respuesta = await fetch("https://api.github.com/users/Greyslor");
+    let datos = await respuesta.json();
+  
+    let $github = document.querySelector("#github");
+  
+    $github.innerHTML = `
+      <table border="1">
+        <tr>
+          <td>Nombre:</td>
+          <td>${datos.name}</td>
+        </tr>
+        <tr>
+          <td>Avatar:</td>
+          <td><img src="${datos.avatar_url}" alt="${datos.name}"></td>
+        </tr>
+        <tr>
+          <td>Email:</td>
+          <td>${datos.email}</td>
+        </tr>
+        <tr>
+          <td>Location:</td>
+          <td>${datos.location}</td>
+        </tr>
+      </table>
+    `;
+  
+    console.log(datos);
+  }
+  
+  obtenerDatosGitHub();
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# ArticuloInvestigacion
-# ArticuloInvestigacion
-# ArticuloInvestigacion
+# Parcial 3 Práctica 1
+## Información de GitHub
+
+- **Nombre**: Grecia Lorena Morales Castillo
+- **Edad**: 20 años
+
+- Correo electrónico
+    - cdmx2510@amerike.edu.mx
+
+### GitHub Pages
+- Artículo Investigación
+    - [@Greyslor](https://github.com/Greyslor/ArticuloInvestigacion)
+
+---
